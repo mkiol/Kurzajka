463ba3977b58d49b3961e94fb3cbadda97fc3393
Muki <michal@mkiol.net>
2017-07-22 13:20:28 +0200

    Minor UI changes

--- source/content-tools.js
+++ source/content-tools.js
@@ -25,16 +25,16 @@ function getIndicator() {
  pIndicator.state = 0
  pTimer = setInterval(()=>{
    switch (pIndicator.state) {
-   case 0: pIndicator.textContent = "....."; break
-   case 1: pIndicator.textContent = "*...."; break
-   case 2: pIndicator.textContent = "**..."; break
-   case 3: pIndicator.textContent = "***.."; break
-   case 4: pIndicator.textContent = "****."; break
-   case 5: pIndicator.textContent = "*****"; break
-   case 6: pIndicator.textContent = "****."; break
-   case 7: pIndicator.textContent = "***.."; break
-   case 8: pIndicator.textContent = "**..."; break
-   case 9: pIndicator.textContent = "*...."; break
+   case 0: pIndicator.textContent = "*....."; break
+   case 1: pIndicator.textContent = ".*...."; break
+   case 2: pIndicator.textContent = "..*..."; break
+   case 3: pIndicator.textContent = "...*.."; break
+   case 4: pIndicator.textContent = "....*."; break
+   case 5: pIndicator.textContent = ".....*"; break
+   case 6: pIndicator.textContent = "....*."; break
+   case 7: pIndicator.textContent = "...*.."; break
+   case 8: pIndicator.textContent = "..*..."; break
+   case 9: pIndicator.textContent = ".*...."; break
    }
    pIndicator.state = (pIndicator.state + 1) % 9
  }, 110)
@@ -48,6 +48,14 @@ function hideToolbar() {
    clearTimeout(pTimer)
    toolbar.parentNode.removeChild(toolbar)
  }
+}
+
+function appendCancel(element) {
+  const butX = document.createElement("button")
+  butX.id = "kurzajka-cancel"
+  butX.textContent = "X"
+  butX.onclick = hideToolbar
+  element.appendChild(butX)
 }
 
 function showToolbar(type) {
@@ -65,13 +73,14 @@ function showToolbar(type) {
      `#kurzajka-toolbar button, input {font-weight:bold; margin: 5px;
      font-size:18px; border: 3px solid ${fgColor}; background-color:${bgColor};
      color:${fgColor}; padding: 2px 4px; vertical-align: middle}
-     #kurzajka-toolbar p, label {margin: 5px; font-weight:bold}
+     #kurzajka-toolbar p, label {margin: 5px; font-weight:bold;}
      #kurzajka-toolbar img {vertical-align:middle; margin-right:5px}
      #kurzajka-toolbar button:hover {background-color:${bgLightColor}}
-     #kurzajka-indicator {font-size:20px; margin:0 5px 0 0; font-family: monospace;}
-     #kurzajka-toolbar {padding:4px 8px; font-size:16px; position:fixed;
-     top:0; left:0; width:100%; color:${fgColor}; z-index:10000;
-     border-bottom: 5px solid ${fgColor}; background-color:${bgColor}}
+     #kurzajka-indicator {font-size:20px; margin:0 5px 0 5px; font-family: monospace;}
+     #kurzajka-toolbar {padding:5px 15px 5px 55px; font-size:16px;
+     position:fixed; top:0; left:0; width:100%; color:${fgColor}; z-index:10000;
+     border-bottom: 5px solid ${fgColor};
+     background: ${bgColor} url("${browser.extension.getURL("res/kurzajka-48.png")}") no-repeat 5px 5px;}
      button#kurzajka-cancel {position: fixed; top:0; right:0; border:hidden}`
    document.getElementsByTagName("head")[0].appendChild(style)
  }
@@ -81,10 +90,6 @@ function showToolbar(type) {
 
  if (type === "start-search-question") {
 
-   const butX = document.createElement("button")
-   butX.id = "kurzajka-cancel"
-   butX.textContent = "X"
-   butX.onclick = hideToolbar
    const p = document.createElement("p")
    p.textContent = browser.i18n.getMessage("startSearchQuestion")
    const butStart = document.createElement("button")
@@ -107,8 +112,8 @@ function showToolbar(type) {
 
    p.appendChild(butStart)
    p.appendChild(labelReg)
-   p.appendChild(butX)
    toolbar.appendChild(p)
+   appendCancel(toolbar)
 
  } else if (type === "autoreg-question") {
 
@@ -119,6 +124,7 @@ function showToolbar(type) {
    butAccept.onclick = ()=>sendMessage({type: "start_search", autoreg: true})
    toolbar.appendChild(p)
    toolbar.appendChild(butAccept)
+   appendCancel(toolbar)
 
  } else if (type === "stop-search-question") {
 
@@ -136,34 +142,25 @@ function showToolbar(type) {
 
    const p = document.createElement("p")
    p.textContent = browser.i18n.getMessage("visitFoundTitle")
-   const butX = document.createElement("button")
-   butX.id = "kurzajka-cancel"
-   butX.textContent = "X"
-   butX.onclick = hideToolbar
-   p.appendChild(butX)
    toolbar.appendChild(p)
+   toolbar.appendChild(document.createElement("br"))
+   appendCancel(toolbar)
 
  } else if (type === "autoreg-error") {
 
    const p = document.createElement("p")
    p.textContent = browser.i18n.getMessage("autoregErrorTitle")
-   const butX = document.createElement("button")
-   butX.id = "kurzajka-cancel"
-   butX.textContent = "X"
-   butX.onclick = hideToolbar
-   p.appendChild(butX)
    toolbar.appendChild(p)
+   toolbar.appendChild(document.createElement("br"))
+   appendCancel(toolbar)
 
  } else if (type === "autoreg-success") {
 
    const p = document.createElement("p")
    p.textContent = browser.i18n.getMessage("autoregSuccessTitle")
-   const butX = document.createElement("button")
-   butX.id = "kurzajka-cancel"
-   butX.textContent = "X"
-   butX.onclick = hideToolbar
-   p.appendChild(butX)
    toolbar.appendChild(p)
+   toolbar.appendChild(document.createElement("br"))
+   appendCancel(toolbar)
  }
 
  document.body.appendChild(toolbar)
